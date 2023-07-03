fc0c05d12b88f0a97b68d2ac4db4bbc13fe7c632
Shahin Endregard Hemat <120106335+ShahinHemat@users.noreply.github.com>
2023-07-03 16:59:13 +0200

    3/4 Creating Project Form complete
    Only missing the handleSubmit functionality (connecting to GraphQL)

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,12 @@
 'use client'
 
 import { SessionInterface } from '@/common.types'
-import React from 'react'
+import Image from 'next/image'
+import React, { ChangeEvent, useState } from 'react'
+import FormField from './FormField'
+import { categoryFilters } from '@/constants'
+import CustomMenu from './CustomMenu'
+import Button from './Button'
 
 type Props = {
     type: string,
@@ -11,14 +16,116 @@ type Props = {
 
 const ProjectForm = ({ type, session }: Props) => {
     const handleFormSubmit = (e: React.FormEvent) => {
+        
+    };
 
-    }
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
 
-  return (
-    <form onSubmit={handleFormSubmit} className='flexStart form'>
+        const file = e.target.files?.[0];
 
-    </form>
-  )
+        if (!file) return;
+
+        if (!file.type.includes('image')) {
+            return alert('Please upload an image file');
+        }
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            const result = reader.result as string;
+
+            handleStateChange('image', result)
+        }
+    };
+
+    const handleStateChange = (fieldName: string, value: string) => {
+        setForm((prevState) => ({ ...prevState, [fieldName]: value }))
+    };
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const [form, setForm] = useState({
+        title: '',
+        description: '',
+        image: '',
+        liveSiteUrl: '',
+        githubUrl: '',
+        category: '',
+    })
+
+    return (
+        <form onSubmit={handleFormSubmit} className='flexStart form'>
+
+            <div className='flexStart form_image-container'>
+                <label htmlFor='poster' className='flexCenter form_image-label'>
+                    {!form.image && 'Choose a poster for your project'}
+                </label>
+                <input id='image' type='file' accept='image/*' required={type === 'create'} className='form_image-input' onChange={handleChangeImage} />
+                {form.image && (
+                    <Image
+                        src={form?.image}
+                        className='sm:p-10 object-contain z-20'
+                        alt='Project Poster'
+                        fill
+                    />
+                )}
+            </div>
+
+            <FormField
+                title='Title'
+                state={form.title}
+                placeholder='Flexibble'
+                setState={(value) => handleStateChange('title', value)}
+            />
+            <FormField
+                title='Description'
+                state={form.description}
+                placeholder='Showcase and discover remarkable developer projects.'
+                setState={(value) => handleStateChange('description', value)}
+            />
+            <FormField
+                type='url'
+                title='Website URL'
+                state={form.liveSiteUrl}
+                placeholder='https://......'
+                setState={(value) => handleStateChange('liveSiteUrl', value)}
+            />
+            <FormField
+                type='url'
+                title='GitHub URL'
+                state={form.githubUrl}
+                placeholder='https://github.com/shahinhemat'
+                setState={(value) => handleStateChange('githubUrl', value)}
+            />
+
+            <CustomMenu
+                title='category'
+                state={form.category}
+                filters={categoryFilters}
+                setState={(value) => handleStateChange('category', value)}
+            />
+
+
+            <div className='flexStart w-full'>
+                <Button
+                    title={
+                        isSubmitting
+                         ? `${type === 'create' ? 'Creating' :
+                          'Editing'}` 
+                         : `${type === 'create' ? 'Create' : 'Edit'}`
+                        }
+                    type='submit'
+                    leftIcon={isSubmitting ?
+                        '' : '/plus.svg'}
+                    isSubmitting={isSubmitting}
+                />
+            </div>
+
+        </form>
+    )
 }
 
 export default ProjectForm
